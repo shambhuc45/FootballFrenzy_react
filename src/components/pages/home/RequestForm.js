096d4b8573c2d10ba41c923ceab591b856bc9b43
Shambhu Chaudhary <shambhu@octopusinfosys.com>
2024-01-09 03:06:12 +0545

    Footer Added

--- src/components/pages/home/RequestForm.js
+++ src/components/pages/home/RequestForm.js
@@ -9,47 +9,53 @@ export default function RequestForm() {
       <div className="section-requestForm row">
         <hr className="hor-line-small" />
         <br />
-        <div className="request-form">
-          <form action="" className="form" onSubmit={handleSubmit}>
-            <h1 className="heading-primary">Request a Product</h1>
-            <div className="form-group">
-              <label htmlFor="product-title" className="label-input">
-                Product Title
-              </label>
-              <br />
-              <input
-                type="text"
-                id="product-title"
-                placeholder="Enter Product Title"
-                className="textbox"
-              />
-            </div>
-            <div className="form-group">
-              <label htmlFor="product-description" className="label-input">
-                Product Description
-              </label>
-              <br />
-              <textarea
-                name="textarea-description"
-                id="product-description"
-                cols="50"
-                rows="3"
-              ></textarea>
-            </div>
-            <div className="form-group">
-              <label htmlFor="product-picture" className="label-input">
-                Product Demo Picture
-              </label>
-              <br />
-              <input
-                type="file"
-                name="product-picture"
-                id="product-picture"
-                placeholder="Please Provide Demo"
-              />
-            </div>
-            <button type="submit">Request</button>
-          </form>
+        <div className="request">
+          <div className="request-form">
+            <form action="" className="form" onSubmit={handleSubmit}>
+              <h1 className="heading-primary">Request a Product</h1>
+              <div className="form-group">
+                <label htmlFor="product-title" className="label-input">
+                  Product Title
+                </label>
+                <br />
+                <input
+                  type="text"
+                  id="product-title"
+                  placeholder="Enter Product Title"
+                  className="textbox form-input"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="product-description" className="label-input">
+                  Product Description
+                </label>
+                <br />
+                <textarea
+                  name="textarea-description"
+                  id="product-description"
+                  rows="3"
+                  placeholder="Describe the product"
+                  className="textarea-description form-input"
+                ></textarea>
+              </div>
+              <div className="form-group">
+                <label htmlFor="product-picture" className="label-input">
+                  Product Demo Picture
+                </label>
+                <br />
+                <input
+                  type="file"
+                  name="product-picture"
+                  id="product-picture"
+                  className="product-picture form-input"
+                />
+              </div>
+              <button type="submit" className="btn-primary btn-request">
+                Request
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </>
